64c1f6617d20ce2fd59442d9bf525eef8828bfd3
takumi1225 <takumi.a.tani@rakuten-sec.co.jp>
2021-08-19 17:36:37 +0900

    見出しを赤文字に変更

--- index.php
+++ index.php
@@ -3,9 +3,14 @@
 <head>
   <meta charset="UTF-8" />
   <title>tech boost</title>
+  <style>
+  h1 {
+    color: red;
+  }
+  </style>
 </head>
   <body>
     <h1>Hello world!!!</h1>
-    <p>はじめてのwebサイトへようこそ</p>
+    <p>CSSの入門！</p>
   </body>
 </html>
